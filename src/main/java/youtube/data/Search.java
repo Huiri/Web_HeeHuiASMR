51f755d377f73c3d37bb40315a6d49c32dc8c194
euni999 <75670831+euni999@users.noreply.github.com>
2022-09-05 16:10:54 +0900

    키워드별 검색

---
+++ src/main/java/youtube/data/Search.java
@@ -0,0 +1,172 @@
+package youtube.data;
+
+/*
+    키워드 검색
+ */
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Prints a list of videos based on a search term.
+ *
+ * @author Jeremy Walker
+ */
+public class Search {
+
+    /** Global instance properties filename. */
+    private static String PROPERTIES_FILENAME = "youtube.properties";
+
+    /** Global instance of the HTTP transport. */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    /** Global instance of the max number of videos we want returned (50 = upper limit per page). */
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 3;
+
+    /** Global instance of Youtube object to make all API requests. */
+    private static YouTube youtube;
+
+
+    /**
+     * Initializes YouTube object to search for videos on YouTube (Youtube.Search.List). The program
+     * then prints the names and thumbnails of each of the videos (only first 50 videos).
+     *
+     * @param args command line args.
+     */
+    public static void main(String[] args) {
+        // Read the developer key from youtube.properties
+        Properties properties = new Properties();
+        try {
+            InputStream in = Search.class.getResourceAsStream("/" + PROPERTIES_FILENAME);
+            properties.load(in);
+           // properties.load(new FileReader("src/file/" + PROPERTIES_FILENAME));
+
+        } catch (IOException e) {
+            System.err.println("There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause()
+                    + " : " + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            /*
+             * The YouTube object is used to make all API requests. The last argument is required, but
+             * because we don't need anything initialized when the HttpRequest is initialized, we override
+             * the interface and provide a no-op function.
+             */
+            youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                public void initialize(HttpRequest request) throws IOException {}
+            }).setApplicationName("youtube-cmdline-search-sample").build();
+
+            // Get query term from user.
+            String queryTerm = getInputQuery();
+
+            YouTube.Search.List search = youtube.search().list(Collections.singletonList("id,snippet"));
+            /*
+             * It is important to set your API key from the Google Developer Console for
+             * non-authenticated requests (found under the Credentials tab at this link:
+             * console.developers.google.com/). This is good practice and increased your quota.
+             */
+            String apiKey = properties.getProperty("youtube.apikey");
+            search.setKey(apiKey);
+            search.setQ(queryTerm);
+            /*
+             * We are only searching for videos (not playlists or channels). If we were searching for
+             * more, we would add them as a string like this: "video,playlist,channel".
+             */
+            search.setType(Collections.singletonList("video"));
+            /*
+             * This method reduces the info returned to only the fields we need and makes calls more
+             * efficient.
+             */
+            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+            SearchListResponse searchResponse = search.execute();
+
+            List<SearchResult> searchResultList = searchResponse.getItems();
+
+            if (searchResultList != null) {
+                prettyPrint(searchResultList.iterator(), queryTerm);
+            }
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    /*
+     * Returns a query term (String) from user via the terminal.
+     */
+    private static String getInputQuery() throws IOException {
+
+        String inputQuery = "";
+
+        System.out.print("Please enter a search term: ");
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        inputQuery = bReader.readLine();
+
+        if (inputQuery.length() < 1) {
+            // If nothing is entered, defaults to "YouTube Developers Live."
+            inputQuery = "YouTube Developers Live";
+        }
+        return inputQuery;
+    }
+
+    /*
+     * Prints out all SearchResults in the Iterator. Each printed line includes title, id, and
+     * thumbnail.
+     *
+     * @param iteratorSearchResults Iterator of SearchResults to print
+     *
+     * @param query Search query (String)
+     */
+    private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+
+        System.out.println("\n=============================================================");
+        System.out.println(
+                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
+        System.out.println("=============================================================\n");
+
+        if (!iteratorSearchResults.hasNext()) {
+            System.out.println(" There aren't any results for your query.");
+        }
+
+        while (iteratorSearchResults.hasNext()) {
+
+            SearchResult singleVideo = iteratorSearchResults.next();
+            ResourceId rId = singleVideo.getId();
+
+            // Double checks the kind is video.
+            if (rId.getKind().equals("youtube#video")) {
+                Thumbnail thumbnail = (Thumbnail) singleVideo.getSnippet().getThumbnails().get("default");
+
+                System.out.println(" Video Id" + rId.getVideoId());
+                System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
+                System.out.println(" Thumbnail: " + thumbnail.getUrl());
+                System.out.println("\n-------------------------------------------------------------\n");
+            }
+        }
+    }
+}
