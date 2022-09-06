433894b4fdec377cdda28ec4ac0d4bfe369c3eb2
Huiri <gmlfltj@naver.com>
2022-09-07 01:06:57 +0900

    feat: comment

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {
     VideoTitle,
     VideoCreator,
@@ -6,19 +6,80 @@ import {
     VideoSub,
     VideoWrapper,
     VideoSection,
+    CommentContainer,
     CommentWrapper,
-    CommentInputSection,
+    CommentInputWrapper,
     CommentTitle,
     CommentInput,
     CommentBtn,
-    CommentSection,
     SubWrapper,
     VideoSubTitle,
     HeartBtn,
-    IconWrapper, PageBtn
+    IconWrapper,
+    PageBtn,
+    CommentSection,
+    CommentText,
+    CommentUser,
+    CommentIconWrapper,
+    CommentItem,
+    GarbageBtn
 } from './styled';
+import CommentList from "../../components/Comment/CommentList";
+import CommentInputs from "../../components/Comment/CommentInput";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {commentListState} from "../../recoil/comment";
+
 const Detail = () => {
-    const [isLike, setIsLike] = useState(false);
+
+    const [isLiked, setIsLiked] = useState(false);
+    const nextId = useRef(0);
+    const [text, setText] = useState("");
+    // const commentList = useRecoilValue(filteredCommentListState);
+    // const setComments = useSetRecoilState(commentListState);
+    // const comments = useRecoilValue(commentListState);
+
+    const [comments, setComments] = useState([]);
+
+
+    // const addComment = () => {
+    //
+    //     setComments((oldComments) => {
+    //         const id = oldComments.length
+    //             ? oldComments[oldComments.length -1].id+1
+    //             :0;
+    //
+    //         //기존 oldComments에 원소가 있으면 id + 1 없으면 id = 0
+    //         return [
+    //             ...oldComments,
+    //             {
+    //                 content: content,
+    //                 id : Math.random() * 100,
+    //                 isCompleted : fa
+    //                 // date : date.getUTCDate()
+    //             },
+    //         ];
+    //     });
+    //     setContent('');
+    //
+    // };
+    const addCommentHandler = e =>{
+        e.preventDefault();
+
+        if(text === '') {
+            alert('내용을 입력해주세요');
+        } else {
+            // setComments(comments => comments.concat({ id: nextId.current + 1, text, completed: false }));
+
+            setComments(commentValueList => [text, ...commentValueList]);
+            setText('');
+            // addComment(content);
+
+        }
+    };
+
+    const handleChange = e => {
+        setText(e.target.value);
+    };
     return (
         <div>
             <TitleWrapper>
@@ -26,7 +87,7 @@ const Detail = () => {
                 <VideoCreator>영상 창작자</VideoCreator>
             </TitleWrapper>
             <IconWrapper>
-                <HeartBtn onClick={()=>setIsLike(!isLike)}/>
+                <HeartBtn onClick={()=>setIsLiked(!isLiked)}/>
                 <PageBtn/>
             </IconWrapper>
             <VideoWrapper>
@@ -38,16 +99,47 @@ const Detail = () => {
 
             </SubWrapper>
             <hr/>
-            <CommentWrapper>
+            <CommentContainer>
                 <CommentTitle>의견 나눔장</CommentTitle>
-                <CommentInputSection>
-                    <CommentInput/>
-                    <CommentBtn>SEND</CommentBtn>
-                </CommentInputSection>
+                <CommentInputWrapper>
+                    <form onSubmit={addCommentHandler}>
+                        <CommentInput type={"text"} value={text} placeholder={"댓글 달기..."} onChange={handleChange}/>
+                        <CommentBtn onClick={addCommentHandler}>SEND</CommentBtn>
+                    </form>
+                </CommentInputWrapper>
                 <CommentSection>
+                    {comments.map((value, id) => (
+                        <CommentItem key={id}>
+                            <CommentWrapper>
+                                <CommentUser>Chaz</CommentUser>
+                                <CommentText>{value}</CommentText>
 
+                            </CommentWrapper>
+
+                            <CommentIconWrapper>
+                                <HeartBtn size={"1.4rem"}/>
+                                <GarbageBtn/>
+                            </CommentIconWrapper>
+                        </CommentItem>
+                    ))}
+                    {/*{comments.map((comment) => (*/}
+                    {/*    <CommentWrapper key={comment.id}>*/}
+                    {/*        <CommentCard {...comment} />*/}
+                    {/*    </CommentWrapper>*/}
+                    {/*))}*/}
+
+                    {/*<CommentListStats />  // 상태값을 보여줄 컴포넌트*/}
+                    {/*<CommentListFiters />  // 필터할 컴포넌트*/}
+                    {/*<CommentItemCreator />*/}
+
+                    {/*{commentList.map((commentItem) => (*/}
+                    {/*    <CommentItem key={commentItem.id} item={commentItem} />*/}
+                    {/*))}*/}
                 </CommentSection>
-            </CommentWrapper>
+                <CommentInputs/>
+                <CommentList/>
+
+            </CommentContainer>
 
 
         </div>
--- src/pages/Detail/styled.js
+++ src/pages/Detail/styled.js
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {ReactComponent as HeartIcon} from '../../assets/icons/HeartIcon.svg';
 import {ReactComponent as PageIcon} from '../../assets/icons/PageIcon.svg';
+import {ReactComponent as GarbageIcon} from '../../assets/icons/GarbageIcon.svg';
 
 //비디오 설명
 export const TitleWrapper = styled.div`
@@ -21,12 +22,17 @@ export const IconWrapper = styled.div`
   margin : -2rem 4rem 0 0;
 `
 export const HeartBtn = styled(HeartIcon)`
-  width: 1.8rem;
-  height: 1.8rem;
+  width: ${props => props.size || "1.8rem"};
+  height: ${props => props.size || "1.8rem"};
   fill: #383838;
   cursor : pointer;
   margin-right : 1.3rem;
-  &:active, &:hover{
+  ${({ isLiked }) =>
+      isLiked &&
+          css`
+      fill : red;
+    `}
+  &:hover{
     fill : red;
   }
   
@@ -46,11 +52,12 @@ export const SubWrapper = styled.div`
 
 `
 export const VideoSubTitle = styled.p`
-  font-size : 1.3rem;
-  font-weight : 600;
+  font-size : 1.4rem;
+  margin-bottom : 0;
 `
 export const VideoSub = styled.p`
-  margin-left : 1rem;
+  font-size: 1.1rem;
+  margin : 1rem 0 3rem 1rem;
 `
 //비디오 출력 부분
 export const VideoWrapper = styled.div`
@@ -66,19 +73,19 @@ export const VideoSection = styled.div`
 `
 
 //댓글 부분
-export const CommentWrapper = styled.div`
+export const CommentContainer = styled.div`
   margin : 0 0 0 3.5rem;
 
 `
 
 export const CommentTitle = styled.h1`
-  margin-bottom : 0.5rem;
+  margin-bottom : 0;
   font-size : 2rem
 
 `
 
 //댓글 입력 부분
-export const CommentInputSection = styled.div`
+export const CommentInputWrapper = styled.div`
   display : flex;
   align-items: center;
 `
@@ -86,7 +93,7 @@ export const CommentInput = styled.input`
   font-size: 1.3rem;
   width: 80vw;
   height: 10vh;
-  margin : 2rem 0;
+  margin : 1rem 0;
   
   &:focus {
   }
@@ -99,4 +106,46 @@ export const CommentBtn = styled.button`
 `
 //실제 댓글창
 export const CommentSection = styled.div`
+  width : 100%;
+  display : flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+`
+
+export const CommentWrapper = styled.div`
+  display : flex;
+  align-items: center;
+`
+
+export const CommentItem = styled.div`
+  margin-bottom : 0.5rem;
+  width : 95%;
+  background: antiquewhite;
+  border-radius: 0.7rem;
+  
+
+`
+
+export const CommentUser = styled.span`
+  font-weight : 600;
+  font-size : 1.2rem;
+  margin-left : 1rem;
+`
+
+export const CommentIconWrapper = styled.div`
+  float : right;
+  margin : -2rem 1rem 0 0;
+`
+export const CommentText = styled.p`
+  margin : 0.7rem;
+`
+
+export const GarbageBtn = styled(GarbageIcon)`
+  fill: #464646;
+  width : 1.4rem;
+  height : 1.4rem;
+  cursor : pointer;
+  &:hover {
+    fill : black;
+  }
 `
