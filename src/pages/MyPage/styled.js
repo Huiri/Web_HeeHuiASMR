b2ff6f9ca9b3dfcb7b8e19cb05fa254224561a0c
Huiri <gmlfltj@naver.com>
2022-09-16 21:06:27 +0900

    feat : mypage, likedpage 레이아웃 추가

---
+++ src/pages/MyPage/styled.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  background: aliceblue;
+  width : 100vw;
+  height : 100vh;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 6fr;
+  grid-template-columns: 1fr 5fr;
+  grid-template-areas:
+    "sidebar main main"
+    "sidebar main main"
+    "sidebar main main"
+    "sidebar main main"
+    "sidebar main main";
+  grid-gap: 0.25rem;
+  transition: all 0.25s ease-in-out;
+
+`
+
+export const PrivacyWrapper = styled.div`
+  margin : 1rem 0 0 1rem;
+`
+
+export const PrivacyTitle = styled.h3`
+`
+
+export const PrivactSub = styled.p`
+    font-size: 1.1rem;
+  font-weight : 600;
+  margin-top : 2rem;
+`
+
+export const PassWordChangeEmail = styled.p`
+  cursor: pointer;
+  margin-left: 0.8rem;
+  display: inline-block;
+
+  &:hover {
+    color: #7c7c7c;
+    transition: all ease 0.3s 0s;
+  }
+
+`
+export const PrivacyName = styled.p``
+export const PrivacyDelete = styled.p`
+  cursor: pointer;
+  font-weight: 600;
+  width : 10%;
+  margin-top : 5rem;
+
+  &:hover {
+    color: #d54444;
+    transition: all ease 0.3s 0s;
+
+  }
+`
+
+export const PrivacyItem = styled.p`
+  margin-left : 0.8rem;
+
+`
+
+export const PrivacyPhoto = styled.p``
+
+export const LoginTitle = styled.h1`
+  text-align : center;
+  
+`
+
+
+export const LoginSub = styled.h1`
+  text-align : center;
+
+`
