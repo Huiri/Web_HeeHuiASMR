6528da66f9f198dc184fad3f14e1fbaddffd10e4
Huiri <74657881+Huiri@users.noreply.github.com>
2022-10-21 15:28:16 +0900

    Update useYoutube.js

--- src/hooks/useYoutube.js
+++ src/hooks/useYoutube.js
@@ -10,11 +10,11 @@ function useFetch(param){
     const PAGE_LIMIT = 50;
     const fetchData = () => {
         axios
-            //AIzaSyCPRq28ZkbXZ6_eJ3nGe9RfxLoO2Cfh7uI
-            //AIzaSyBmNXK-4gvjD7785WFaQVbuGCQwWErPKUA
+            //Google_API_KEY
+            //Google_API_KEY
             .get(
-                // `https://www.googleapis.com/youtube/v3/search?part=snippet&q=ASMR${param}&maxResults=4&type=video&regionCode=KR&key=AIzaSyCHAdXUjuGX9fznEeA6Fz6EHpABipgxN98`
-                `https://www.googleapis.com/youtube/v3/search?part=snippet&q=먹방${param}&maxResults=9&type=video&regionCode=KR&key=AIzaSyBmNXK-4gvjD7785WFaQVbuGCQwWErPKUA`
+                // `https://www.googleapis.com/youtube/v3/search?part=snippet&q=ASMR${param}&maxResults=4&type=video&regionCode=KR&key=Google_API_KEY`
+                `https://www.googleapis.com/youtube/v3/search?part=snippet&q=먹방${param}&maxResults=9&type=video&regionCode=KR&key=Google_API_KEY`
             )
             .then((res) => {
                 //console.log(res);
